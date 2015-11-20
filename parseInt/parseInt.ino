2f8ae1436f2f4c0916b07a176d6839d830c31773
nokia6102 <nokia6102@gmail.com>
2015-11-20 21:20:40 +0800

    My Soultion
    Splite RGB

--- parseInt/parseInt.ino
+++ parseInt/parseInt.ino
@@ -1,4 +1,5 @@
 long x = 0;
+int r,g,b;
 void setup()
 {
   Serial.begin(9600);
@@ -13,5 +14,11 @@ void loop()
     Serial.print("your input is: ");
     Serial.println(x);
     delay(500);
+    r=x/1000/1000;
+    g=x/1000%1000;
+    b=x%1000;
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
   }
 }
